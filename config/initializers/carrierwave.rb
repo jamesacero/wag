3612fde3f0c09b9de3915736b0aea4e2726bb0cf
James Acero <james.acero@gmail.com>
2015-10-12 19:40:24 -0400

    Fixed hound errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_KEY_ID'] || "",
-    aws_secret_access_key: ENV['AWS_KEY'] || "",
+    provider: "AWS",
+    aws_access_key_id: ENV["AWS_KEY_ID"] || "",
+    aws_secret_access_key: ENV["AWS_KEY"] || "",
   }
-  config.fog_directory  = ENV['AWS_BUCKET']
+  config.fog_directory  = ENV["AWS_BUCKET"]
 end
