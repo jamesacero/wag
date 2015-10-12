b7c4976cd0a613fdf29cab49dc8185b044153c1a
James Acero <james.acero@gmail.com>
2015-10-12 11:39:25 -0400

    added styling to nav-bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.order(created_at: :desc)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+      flash[:notice] = "User Created!"
+    else
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  devise_scope :user do
-    get  'sitters/sign_up' => 'devise/registrations#new_sitter', :as => 'new_sitter_registration'
-    post 'sitters/sign_up' => 'devise/registrations#create', :as => 'sitter_registration'
-  end
 
+  resources :users, only: [:index, :show, :destroy]
   resources :dogs
   resources :profiles, only: [:index, :new, :create, :show, :edit, :update]
 end
