268d93e5f3ff3451d09f8d701d073703e25a1b9e
James Acero <james.acero@gmail.com>
2015-10-15 11:37:25 -0400

    fixing S3 bucket

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: "test@example.com", password: "exampleuser")
+User.create(email: "jordan@blah.com", password: "exampleuser")
