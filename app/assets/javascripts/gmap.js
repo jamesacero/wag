c4ca98c4a55bdd6e75409f315d838a69b5759b17
James Acero <james.acero@gmail.com>
2015-11-19 13:28:12 -0500

    Updated google maps, started dogpark search, styling

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -1,20 +1,23 @@
+var lat = parseFloat($("#lat").text());
+var lon = parseFloat($("#lon").text());
+var myCenter=new google.maps.LatLng(lat, lon);
+
 function initialize() {
   var mapOptions = {
-      center: new google.maps.LatLng(42.382931, -71.098577),
-      zoom: 14,
+      center: myCenter,
+      zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions); markerFromDB;
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
   //Setting a marker
   var markerOptions = {
-     position: new google.maps.LatLng(42.382931, -71.098577)
+     position: new google.maps.LatLng(lat, lon)
   };
   var marker = new google.maps.Marker(markerOptions);
+
   marker.setMap(map);
-
-
 }
 
 // Run the initialize function when the window has finished loading.
