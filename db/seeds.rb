1c46af3b29334553f02aaee5ea1896720efc7f68
James Acero <james.acero@gmail.com>
2015-11-22 18:55:21 -0500

    Added dogpark pictures to park index

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@ lng: -71.112742,
 name: "Brookline Avenue Playground",
 address: "575 Brookline Ave",
 city: "Brookline",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/brookline.jpg")
 
 Dogpark.create(
 lat: 42.380030,
@@ -20,7 +21,8 @@ lng: -71.082093,
 name: "Zero New Washington Street Park",
 address: "0 New Washington St",
 city: "Somerville",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/0newwash.jpg")
 
 Dogpark.create(
 lat: 42.399902,
@@ -28,7 +30,8 @@ lng: -71.145354,
 name: "Thorndike Field",
 address: "99 Margaret St",
 city: "Arlington",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/thorndike.jpg")
 
 Dogpark.create(
 lat: 42.382721,
@@ -36,7 +39,8 @@ lng: -71.098452,
 name: "Nunziato Field",
 address: "Putnam & Summer St",
 city: "Somerville",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/nunziato.jpg")
 
 Dogpark.create(
 lat: 42.387140,
@@ -44,7 +48,8 @@ lng: -71.093739,
 name: "Edward L Leathers Community Park",
 address: "Skilton Ave",
 city: "Somerville",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/leathers.jpg")
 
 Dogpark.create(
 lat: 42.389634,
@@ -52,7 +57,8 @@ lng: -71.137988,
 name: "Danehy Park",
 address: "99 Sherman St",
 city: "Cambridge",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/daheny.jpg")
 
 Dogpark.create(
 lat: 42.381468,
@@ -60,7 +66,8 @@ lng: -71.145350,
 name: "Kingsley Park",
 address: "200 Fresh Pond Pkwy",
 city: "Cambridge",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/kingsley.jpg")
 
 Dogpark.create(
 lat: 42.330814,
@@ -68,7 +75,8 @@ lng: -71.048723,
 name: "South Boston Bark Park",
 address: "1280 Columbia Rd",
 city: "Boston",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/sb.jpg")
 
 Dogpark.create(
 lat: 42.343484,
@@ -76,7 +84,8 @@ lng: -71.067435,
 name: "Peter's Park Dog Run",
 address: "1277 Washington St",
 city: "Boston",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/peters.jpg")
 
 Dogpark.create(
 lat: 42.379540,
@@ -84,7 +93,8 @@ lng: -71.103320,
 name: "Perry Park",
 address: "355 Washington St",
 city: "Somerville",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/perry.jpg")
 
 Dogpark.create(
 lat: 42.368202,
@@ -92,7 +102,8 @@ lng: -71.194528,
 name: "Watertown Dog Park",
 address: "65 Bacon St",
 city: "Watertown",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/watertown.jpg")
 
 Dogpark.create(
 lat: 42.318272,
@@ -100,7 +111,8 @@ lng: -71.106834,
 name: "Beecher St Dog Park",
 address: "16 Beecher St",
 city: "Jamaica Plain",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/beecher.jpg")
 
 Dogpark.create(
 lat: 42.306939,
@@ -108,7 +120,8 @@ lng: -71.120336,
 name: "Arnold Arboretum - Peter's Hill",
 address: "125 Arborway",
 city: "Jamaica Plain",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/arnold.jpg")
 
 Dogpark.create(
 lat: 42.451951,
@@ -116,7 +129,8 @@ lng: -71.106903,
 name: "Sheepfold Off Leash Area",
 address: "Middlesex Fells",
 city: "Stoneham",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/middlesex.jpg")
 
 Dogpark.create(
 lat: 42.336756,
@@ -124,7 +138,8 @@ lng: -71.143524,
 name: "Jean B Waldstein Playground",
 address: "Dean Rd",
 city: "Brookline",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/waldstein.jpg")
 
 Dogpark.create(
 lat: 42.355269,
@@ -132,7 +147,8 @@ lng: -71.065617,
 name: "Boston Commons",
 address: "Charles St and Boylston St",
 city: "Boston",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/common.jpg")
 
 Dogpark.create(
 lat: 42.345765,
@@ -140,7 +156,8 @@ lng: -71.113426,
 name: "Amory Playground",
 address: "Amory St",
 city: "Brookline",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/amory.jpg")
 
 Dogpark.create(
 lat: 42.330944,
@@ -148,7 +165,8 @@ lng: -71.213370,
 name: "Old Cold Spring Park",
 address: "Beacon St",
 city: "Newton",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/coldspring.jpg")
 
 Dogpark.create(
 lat: 42.585631,
@@ -156,7 +174,8 @@ lng: -70.909492,
 name: "Paddles Park",
 address: "46 L P Henderson Rd",
 city: "Beverly",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/paddles.jpg")
 
 Dogpark.create(
 lat: 42.372925,
@@ -164,7 +183,8 @@ lng: -71.083868,
 name: "Gold Star Mothers Park",
 address: "Gore St",
 city: "East Cambridge",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/gold.jpg")
 
 Dogpark.create(
 lat: 42.304089,
@@ -172,7 +192,8 @@ lng: -71.061430,
 name: "Ronan Dog Park",
 address: "135 Adams St",
 city: "Dorchester",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/ronan.jpg")
 
 Dogpark.create(
 lat: 42.370299,
@@ -180,7 +201,8 @@ lng: -71.061991,
 name: "Paul Revere Park",
 address: "Water St",
 city: "Boston",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/paul1.jpg")
 
 Dogpark.create(
 lat: 42.386627,
@@ -188,7 +210,8 @@ lng: -71.127430,
 name: "Raymond Park",
 address: "Raymond St",
 city: "Cambridge",
-state: "MA")
+state: "MA",
+park_picture: "/dogparks/raymond.jpg")
 
 User.create(
   email: "onescruffian1@gmail.com",
